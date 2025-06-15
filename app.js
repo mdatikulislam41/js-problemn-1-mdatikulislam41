d966a187fd435693ac9604038fc757862b1d2b8a
mdatikulislam41 <mdatikulislam41@gmail.com>
2025-06-15 19:56:31 +0600

    upload with 8 problem

---
+++ app.js
@@ -0,0 +1,55 @@
+/* JavaScript Basic Problem Solving 1 */
+//Problem 1
+//Create a variable called carName, assign the value Volvo to it.
+
+let carName = "Volvo";
+
+//Problem 2
+//On one single line, declare three variables with the following names and values:
+
+let firstName = "John";  
+let lastName = "Doe"
+let age = 35 ; 
+
+//Problem 3
+//Use the correct assignment operator that will result in x being 50 (same as x = x * y).
+
+let x = 10;
+let y = 5;
+x *= y  
+console.log(x);
+
+//Problem 4
+//Use comments to describe the correct data type of the following variables:
+
+let length = 16; // Number 
+let lastName = "Johnson"; // String
+
+const x = {
+  firstName: "John",  
+  lastName: "Doe"
+};    // Object
+
+//Problem 5
+//Execute the function named myFunction.
+function myFunction() {
+    alert("Hello World!");
+}
+myFunction();
+
+//Problem 6
+//Create an object called person with name = John, age = 50, Then, access the object to alert("John is 50").
+
+let person = {
+    name: "John",
+    age: 50
+}
+alert(person.name + " is " + person.age);
+
+//Problem 7
+//The <button> element should do something when someone clicks on it. Try to fix it!
+
+<button onclick="alert('clicked')">Click me.</button> 
+
+//Problem 8
+//Array Related Question 
